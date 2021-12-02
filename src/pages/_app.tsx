bfc45c593dc50572f7c894a0d059207ffa30b1dd
Jonathan Corral <johnlejardinnier@gmail.com>
2021-12-02 12:03:40 +0100

    refactor(pages/app): use layouts from unagui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
 
-import '../styles/globals.css';
 import { Wrapper } from '@hoomies/unagui.theme.wrapper';
-import { AppProps } from '@hoomies/noak.types.next';
+import { AppProps as CustomAppProps } from '@hoomies/noak.types.next';
 import { RootLayout } from '@hoomies/noak.layouts.root';
+import { PageLayout as DefaultPageLayout } from '@hoomies/noak.layouts.page.default';
+import {Header} from '@hoomies/unagui.components.header';
+import {Footer} from '@hoomies/unagui.components.footer';
 
+import '../styles/globals.css';
 import theme from '~theme';
-import DefaultPageLayout from '~layouts/page/Default';
 
-import DefaultHeader from '~components/header/Default';
-import DefaultFooter from '~components/footer/Default';
-
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: CustomAppProps) {
   const PageLayout = React.useMemo(() => {
     return Component.pageLayout ?? DefaultPageLayout;
   }, [Component.pageLayout]);
 
   return (
     <Wrapper resetCSS={true} theme={theme}>
-      <RootLayout header={<DefaultHeader />} footer={<DefaultFooter />} {...Component.rootProps}>
+      <RootLayout header={<Header />} footer={<Footer />} {...Component.rootProps}>
         <PageLayout>
           <Component {...pageProps} />
         </PageLayout>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,8 @@ import type { NextPage } from '@hoomies/noak.types.next';
 import Head from 'next/head';
 import Image from 'next/image';
 
-import CustomLayout from '~layouts/Custom';
-
-const Home: NextPage = () => {
-  return <></>;
+const Home: NextPage = (props) => {
+  return <>Hello World!</>;
 };
-
-Home.pageLayout = CustomLayout;
 
 export default Home;
