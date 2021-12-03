52c571a6b2783ff1545ec9a8975f743c587e57c4
Jonathan Corral <johnlejardinnier@gmail.com>
2021-12-03 20:01:02 +0100

    feat(config): create default routes

---
+++ config/routes/custom.js
@@ -0,0 +1,22 @@
+/**
+ * Configures Routes to display in menu
+ */
+const CustomRoutes = [
+  {
+    label: "menu:page.about",
+    path: "/about",
+    icon: "",
+  },
+  {
+    label: "menu:page.blog",
+    path: "/blog",
+    icon: "",
+  },
+  {
+    label: "menu:page.contact",
+    path: "/contact",
+    icon: "",
+  },
+];
+
+module.exports = CustomRoutes;
---
+++ config/routes/default.lock.js
@@ -0,0 +1,13 @@
+// !!! YOU MAY NOT EDIT THIS FILE IN CHILD NOAK-THEME !!!
+/**
+ * Configures Routes to display in menu
+ */
+const DefaultRoutes = [
+  {
+    label: "menu:page.home",
+    path: "/",
+    icon: "",
+  },
+];
+
+module.exports = DefaultRoutes;
