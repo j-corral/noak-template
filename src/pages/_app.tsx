3f8afadd19021833db4a07493182d2c86eb77c51
Jonathan Corral <johnlejardinnier@gmail.com>
2021-12-03 20:04:07 +0100

    refactor(pages/app): refactor dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,10 @@ import React from 'react';
 
 import { Wrapper } from '@hoomies/unagui.theme.wrapper';
 import { AppProps as CustomAppProps } from '@hoomies/noak.types.next';
-import { RootLayout } from '@hoomies/noak.layouts.root';
 import { PageLayout as DefaultPageLayout } from '@hoomies/noak.layouts.page.default';
-import { Header } from '@hoomies/unagui.components.header';
-import { Footer } from '@hoomies/unagui.components.footer';
-import { LocaleSwitcher } from '@hoomies/noak.components.switcher.locale';
 
-import '../styles/globals.css';
-import theme from '~theme';
+import RootLayout from '@/layouts/root';
+import theme from '@/theme';
 
 function MyApp({ Component, pageProps }: CustomAppProps) {
   const PageLayout = React.useMemo(() => {
@@ -18,11 +14,7 @@ function MyApp({ Component, pageProps }: CustomAppProps) {
 
   return (
     <Wrapper resetCSS={true} theme={theme}>
-      <RootLayout
-        header={<Header localeSwitcher={<LocaleSwitcher variant="flushed" label="Change language" />} />}
-        footer={<Footer />}
-        {...Component.rootProps}
-      >
+      <RootLayout {...Component.rootProps}>
         <PageLayout>
           <Component {...pageProps} />
         </PageLayout>
