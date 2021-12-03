47224d7a2a0e911fdb113f0d93978f7f8d5889c4
Jonathan Corral <johnlejardinnier@gmail.com>
2021-12-03 20:02:59 +0100

    feat(components/footer): create default footer component

---
+++ src/components/Footer/Default.tsx
@@ -0,0 +1,12 @@
+// !!! YOU MAY NOT EDIT THIS FILE IN CHILD NOAK-THEME !!!
+import React from 'react';
+
+import { Footer as NoakFooter, FooterProps } from '@hoomies/unagui.components.footer';
+
+export function Footer(props: FooterProps) {
+  return (
+    <>
+      <NoakFooter {...props} />
+    </>
+  );
+}
