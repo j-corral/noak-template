90c1669d5c76b14a5d753a3d575002d1fd72b5ad
Jonathan Corral <johnlejardinnier@gmail.com>
2021-12-07 12:35:27 +0100

    feat(components/header): append ThemeSwitcher inside default header

--- src/components/Header/Default.tsx
+++ src/components/Header/Default.tsx
@@ -1,21 +1,27 @@
 // !!! YOU MAY NOT EDIT THIS FILE IN CHILD NOAK-THEME !!!
 import React from 'react';
 import Image from 'next/image';
+import useTranslation from 'next-translate/useTranslation';
 
 import { Header as NoakHeader, HeaderProps } from '@hoomies/unagui.components.header';
 import { LocaleSwitcher } from '@hoomies/noak.components.switcher.locale';
+import { ThemeSwitcher } from '@hoomies/noak.components.switcher.theme';
 import { Menu } from '@hoomies/noak.components.navigation.menu';
 
 import DefaultRoutes from '~config/routes/default.lock';
 import Logo from '~public/vercel.svg';
 
 export function Header(props: HeaderProps) {
+  const { t } = useTranslation();
+  const localeSwitcherItem = <LocaleSwitcher label={t('menu:lang.text')} variant="outline" />;
+
   return (
     <>
       <NoakHeader
-        logo={<Image src={Logo} width="100%" height="50px" />}
-        localeswitcher={<LocaleSwitcher variant="flushed" label="Change language" variant="outline" />}
+        logo={<Image src={Logo} width="100%" height="50px" alt="logo" />}
         menu={<Menu routes={DefaultRoutes} />}
+        localeswitcher={localeSwitcherItem}
+        themeswitcher={<ThemeSwitcher />}
         p="0 2rem"
         {...props}
       />
