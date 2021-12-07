7b28ce118b5e686380beb0797a3bf66961935442
Jonathan Corral <johnlejardinnier@gmail.com>
2021-12-07 17:28:30 +0100

    feat(components/footer): set default copyright and credits from translation

--- src/components/Footer/Default.tsx
+++ src/components/Footer/Default.tsx
@@ -1,12 +1,25 @@
 // !!! YOU MAY NOT EDIT THIS FILE IN CHILD NOAK-THEME !!!
 import React from 'react';
+import useTranslation from 'next-translate/useTranslation';
 
 import { Footer as NoakFooter, FooterProps } from '@hoomies/unagui.components.footer';
 
 export function Footer(props: FooterProps) {
+  const { t } = useTranslation();
+  const copyright = {
+    author: t('common:copyright.author'),
+    beginYear: Number(t('common:copyright.year')),
+    extra: t('common:copyright.extra'),
+  };
+  const credits = [
+    { text: t('common:powered.text'), name: t('common:powered.by'), url: t('common:powered.url') },
+    { text: t('common:developer.text'), name: t('common:developer.name'), url: t('common:developer.url') },
+    { text: t('common:designer.text'), name: t('common:designer.name'), url: t('common:designer.url') },
+  ];
+
   return (
     <>
-      <NoakFooter {...props} />
+      <NoakFooter copyright={copyright} credits={credits} {...props} />
     </>
   );
 }
