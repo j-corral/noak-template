3a031853328d5961fe1d31653543461253e0f69e
Jonathan Corral <johnlejardinnier@gmail.com>
2021-12-08 13:27:25 +0100

    fix(components/header): update header

--- src/components/Header/Default.tsx
+++ src/components/Header/Default.tsx
@@ -1,7 +1,8 @@
 // !!! YOU MAY NOT EDIT THIS FILE IN CHILD NOAK-THEME !!!
 import React from 'react';
-import Image from 'next/image';
+import NextImage from 'next/image';
 import useTranslation from 'next-translate/useTranslation';
+import { Box, Heading } from '@chakra-ui/react';
 
 import { Header as NoakHeader, HeaderProps } from '@hoomies/unagui.components.header';
 import { LocaleSwitcher } from '@hoomies/noak.components.switcher.locale';
@@ -9,7 +10,7 @@ import { ThemeSwitcher } from '@hoomies/noak.components.switcher.theme';
 import { Menu } from '@hoomies/noak.components.navigation.menu';
 
 import Routes from '~config/routes/header';
-import Logo from '~public/vercel.svg';
+import Logo from '~public/logo.svg';
 
 export function Header(props: HeaderProps) {
   const { t } = useTranslation();
@@ -17,7 +18,11 @@ export function Header(props: HeaderProps) {
   return (
     <>
       <NoakHeader
-        logo={<Image src={Logo} width="100%" height="50px" alt="logo" />}
+        logo={
+          <Box>
+            <NextImage src={Logo} width="50px" height="80px" alt="logo" />
+          </Box>
+        }
         menu={<Menu routes={Routes} />}
         p="0 2rem"
         {...props}
