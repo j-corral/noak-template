f5a47f19e2bcf52dd266ac01628e6636b69d50fe
Jonathan Corral <johnlejardinnier@gmail.com>
2021-12-11 20:32:30 +0100

    feat(config/next): seo rewrite legal page url

--- config/next/rewrites.js
+++ config/next/rewrites.js
@@ -1,6 +1,14 @@
 // https://nextjs.org/docs/api-reference/next.config.js/rewrites
 const NextRewrites = [
-    
+    {
+        source: '/mentions-legales',
+        destination: '/legal',
+    },
+    {
+        source: '/es/notas-legales',
+        destination: '/es/legal',
+        locale: false
+    },
 ];
 
 module.exports = NextRewrites;
