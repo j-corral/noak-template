db08b3a7df05d3090f5f80c86d9f84768bb3bc2a
Jonathan Corral <johnlejardinnier@gmail.com>
2021-12-13 20:34:21 +0100

    feat(config/next): auto rewrite configured routes for better seo

--- config/next/rewrites.js
+++ config/next/rewrites.js
@@ -1,14 +1,41 @@
+function autoRewriteRoutes() {
+    const routes = require('../routes');
+    const i18n = require('../../i18n.js');
+
+    console.log('Rewriting routes...');
+    
+    const defaultLocale = i18n.defaultLocale ?? 'fr';
+    let rewrites = [];
+    routes.forEach(route => {
+        if(route?.lang !== undefined) {
+            Object.keys(route.lang).forEach(key => {
+                const srcRoute = route.lang[key].replace(/\//g, '');
+                const dstRoute = route.path.replace(/(\/?)(.*)/, '$2');
+                const prefix = (key !== defaultLocale ? `/${key}` : '');
+                const rewrite = {
+                    source: `${prefix}/${srcRoute}`,
+                    destination: `${prefix}/${dstRoute}`,
+                    locale: prefix.length > 0 ? false : undefined,
+                };
+                rewrites.push(rewrite);
+            });
+        }
+    });
+
+    return rewrites;
+}
+
 // https://nextjs.org/docs/api-reference/next.config.js/rewrites
-const NextRewrites = [
-    {
-        source: '/mentions-legales',
-        destination: '/legal',
-    },
-    {
-        source: '/es/notas-legales',
-        destination: '/es/legal',
-        locale: false
-    },
-];
+const NextRewrites = {
+    beforeFiles: [
+        ...autoRewriteRoutes(),
+    ],
+    fallback: [
+        {
+            source: '/:page',
+            destination: '/p/:page',
+        },
+    ],
+};
 
 module.exports = NextRewrites;
