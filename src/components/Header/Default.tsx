2164f18a58a98a1f8a1affe97c0b0f8dbc20bd19
Jonathan Corral <johnlejardinnier@gmail.com>
2021-12-15 19:38:42 +0100

    fix(components/header): rewrite component to optimize seo and perf

--- src/components/Header/Default.tsx
+++ src/components/Header/Default.tsx
@@ -1,5 +1,5 @@
 // !!! YOU MAY NOT EDIT THIS FILE IN CHILD NOAK-THEME !!!
-import React from 'react';
+import React, { useMemo } from 'react';
 import NextImage from 'next/image';
 import useTranslation from 'next-translate/useTranslation';
 import { Box } from '@chakra-ui/react';
@@ -9,7 +9,7 @@ import { LocaleSwitcher } from '@hoomies/noak.components.switcher.locale';
 import { ThemeSwitcher } from '@hoomies/noak.components.switcher.theme';
 import { Menu } from '@hoomies/noak.components.navigation.menu';
 
-import Routes from '~config/routes';
+import { getRoute } from '~config/routes';
 import HeaderRoutes from '~config/routes/header';
 import Logo from '~public/logo.svg';
 
@@ -19,22 +19,33 @@ export function Header(props: HeaderProps) {
   const { t } = useTranslation();
   const { asPath } = useRouter();
 
-  const CurrentRoute = Routes.find((route) => route.path === asPath) ?? false;
+  const label = t('menu:lang.text');
+
+  const logo = useMemo(
+    () => (
+      <Box>
+        <NextImage src={Logo} width="50px" height="80px" alt="logo" />
+      </Box>
+    ),
+    [],
+  );
+  const menu = useMemo(() => <Menu routes={HeaderRoutes} />, []);
+
+  const themeSwitcher = useMemo(() => <ThemeSwitcher />, []);
+
+  const localeSwitcher = useMemo(() => {
+    const route = getRoute(asPath);
+    if (typeof route === 'object') {
+      return <LocaleSwitcher route={route} label={label} variant="outline" />;
+    }
+    return <LocaleSwitcher label={label} variant="outline" />;
+  }, [asPath, label]);
 
   return (
     <>
-      <NoakHeader
-        logo={
-          <Box>
-            <NextImage src={Logo} width="50px" height="80px" alt="logo" />
-          </Box>
-        }
-        menu={<Menu routes={HeaderRoutes} />}
-        p="0 2rem"
-        {...props}
-      >
-        <LocaleSwitcher route={CurrentRoute} label={t('menu:lang.text')} variant="outline" />
-        <ThemeSwitcher />
+      <NoakHeader logo={logo} menu={menu} p="0 2rem" {...props}>
+        {localeSwitcher}
+        {themeSwitcher}
       </NoakHeader>
     </>
   );
