1805ddae242a628d25866997afde835231d23145
Jonathan Corral <johnlejardinnier@gmail.com>
2022-02-20 11:47:56 +0100

    fix(src/lib): define types

--- src/lib/mdx.ts
+++ src/lib/mdx.ts
@@ -3,11 +3,11 @@ import path from 'path';
 
 const root = process.cwd();
 
-export async function getFiles(type) {
+export async function getFiles(type: string) {
   return fs.readdirSync(path.join(root, 'data', type));
 }
 
-export async function getSourceFileBySlug(type, slug) {
+export async function getSourceFileBySlug(type: string, slug: string) {
   const source = slug
     ? fs.readFileSync(path.join(root, 'data', type, `${slug}.mdx`), 'utf8')
     : fs.readFileSync(path.join(root, 'data', `${type}.mdx`), 'utf8');
